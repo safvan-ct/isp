951c3e90967a52e63542cf9344cab956d73f330f
safvan-ct <safvanctsfn@gmail.com>
2025-07-19 07:11:12 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Include the head
+fetch("layouts/head.html")
+	.then((res) => res.text())
+	.then((data) => {
+		document.head.innerHTML += data;
+	})
+	.catch((err) => console.error("Error loading head:", err));
+
+// Include the navbar
+fetch("layouts/nav.html")
+	.then((res) => res.text())
+	.then((data) => {
+		document.getElementById("navbar-container").innerHTML = data;
+
+		// Highlight the active link
+		const currentPage = location.pathname.split("/").pop();
+		const cleanedPage = currentPage.replace("-qa", "");
+
+		const navLinks = document.querySelectorAll(".nav-link");
+		navLinks.forEach((link) => {
+			if (link.getAttribute("href") === cleanedPage) {
+				link.classList.add("active");
+			}
+		});
+	})
+	.catch((err) => console.error("Error loading navbar:", err));
+
+// Include the footer
+fetch("layouts/footer.html")
+	.then((res) => res.text())
+	.then((data) => {
+		document.getElementById("footer-container").innerHTML = data;
+	})
+	.catch((err) => console.error("Error loading navbar:", err));
