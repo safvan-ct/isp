c5faa4440728027be19c45fa8d75d29169304aa0
safvan-ct <safvanctsfn@gmail.com>
2025-07-27 23:04:54 +0530

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,6 +42,22 @@ fetch("partials/footer.html")
 	})
 	.catch((err) => console.error("Error loading head:", err));
 
+function handleShare() {
+	const url = window.location.href;
+	if (navigator.share) {
+		navigator.share({
+			title: document.title,
+			text: "Check out this page:",
+			url: url,
+		});
+	} else {
+		navigator.clipboard
+			.writeText(url)
+			.then(() => alert("URL copied to clipboard!"))
+			.catch(() => alert("Copy failed. Please copy manually."));
+	}
+}
+
 async function saveData(fs_db, filePath) {
 	const res = await fetch(filePath);
 	const topics = await res.json();
