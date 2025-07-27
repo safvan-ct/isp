2574e609e1f9bbb50befe172e3554f6a79173cde
safvan-ct <safvanctsfn@gmail.com>
2025-07-27 14:27:02 +0530

    style fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,12 +15,17 @@ fetch("partials/navbar.html")
 		// Highlight the active link
 		const currentPage = location.pathname.split("/").pop();
 		const cleanedPage = currentPage.replace(/-.*$/, "").replace(".html", "");
-		const route = cleanedPage === "index" || cleanedPage === "" ? "home" : cleanedPage;
+		const route =
+			cleanedPage === "index" || cleanedPage === "" ? "home" : cleanedPage;
 
 		const navLinks = document.querySelectorAll(".nav-link");
 		navLinks.forEach((link) => {
 			const linkText = link.textContent.trim().toLowerCase();
-			const linkHref = link.getAttribute("href").split("/").pop().replace(".html", "");
+			const linkHref = link
+				.getAttribute("href")
+				.split("/")
+				.pop()
+				.replace(".html", "");
 
 			if (linkText === route.toLowerCase() || linkHref === route) {
 				link.classList.add("active");
@@ -36,3 +41,42 @@ fetch("partials/footer.html")
 		document.getElementById("footerContainer").innerHTML = data;
 	})
 	.catch((err) => console.error("Error loading head:", err));
+
+// async function saveData() {
+//     fetch("db/life.json")
+//     const res = await fetch("db/life.json");
+//     const topics = await res.json();
+
+//     for (const topic of topics) {
+//         const ref = db.collection("life_of_muslim").doc(topic.id.toString());
+//         await ref.set(topic);
+//         console.log("Uploaded:", topic.title);
+//     }
+// }
+
+// async function exportCollectionToJson(collectionName) {
+//     try {
+//         const snapshot = await db.collection(collectionName).get();
+//         const data = [];
+
+//         snapshot.forEach(doc => {
+//             data.push({ id: doc.id, ...doc.data() });
+//         });
+
+//         // Convert to JSON string
+//         const jsonStr = JSON.stringify(data, null, 2);
+
+//         // Trigger file download
+//         const blob = new Blob([jsonStr], { type: "application/json" });
+//         const url = URL.createObjectURL(blob);
+//         const a = document.createElement("a");
+//         a.href = url;
+//         a.download = `${collectionName}.json`;
+//         a.click();
+//         URL.revokeObjectURL(url);
+
+//         console.log("✅ Exported:", collectionName);
+//     } catch (err) {
+//         console.error("❌ Export failed:", err);
+//     }
+// }
